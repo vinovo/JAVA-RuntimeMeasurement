7505995981090bb849df734256ea3dedc4aba00a
Nik <nsankara@ucsd.edu>
2018-02-01 12:31:37 -0800

    removed implementations

--- pa4-student/src/cse12pa4student/Measure.java
+++ pa4-student/src/cse12pa4student/Measure.java
@@ -5,37 +5,23 @@ import java.util.List;
 import static cse12pa4mysteries.Mysteries.*;
 
 public class Measure {
-	public static long time(String m, int n) {
-		long start = System.nanoTime();
-		if(m.equals("A")) { mysteryA(n); }
-		else if(m.equals("B")) { mysteryB(n); }
-		else if(m.equals("C")) { mysteryC(n); }
-		else if(m.equals("D")) { mysteryD(n); }
-		else if(m.equals("E")) { mysteryE(n); }
-		else if(m.equals("F")) { mysteryF(n); }
-		long end = System.nanoTime();
-		return end - start;
-	}
+
+
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
-		List<Measurement> lst = new ArrayList<>();
-		for(int i = startN; i <= stopN; i += 1) {
-			for(String s: toRun) {
-				long result = time(s, i);
-				lst.add(new Measurement(s, i, result));
-			}
-		}
-		return lst;
+		/** TODO **/
+		return null;
 	}
+	
+
 	public static String measurementsToCSV(List<Measurement> toConvert) {
-		String s = "name,n,nanoseconds";
-		for(Measurement m: toConvert) {
-			s += m.name + "," + m.valueOfN + "," + m.nanosecondsToRun + "\n";
-		}
-		return s;
+		/** TODO **/
+		return null;
 	}
+	
+	/* Add any helper methods you find useful */
+		
+	
 	public static void main(String[] args) {
-		System.out.println("Running");
-		List<Measurement> l = measure(new String[]{"a", "c"}, 40, 100);
-		System.out.println(l);
+		
 	}
 }
--- pa4-student/src/cse12pa4student/Measurement.java
+++ pa4-student/src/cse12pa4student/Measurement.java
@@ -1,15 +1,22 @@
+/* 
+ * DO NOT MODIFY
+ * Class definition to assist in measuring method execution time 
+ */
+
 package cse12pa4student;
 
 public class Measurement {
-	public final String name;
-	public final int valueOfN;
-	public final long nanosecondsToRun;
+	public final String name;				//name of mystery method
+	public final int valueOfN;				//input value to method
+	public final long nanosecondsToRun;		//execution time in ns
+	
+	/* Constructor */
 	public Measurement(String name, int valueOfN, long nanosecondsToRun) {
 		this.name = name;
 		this.valueOfN = valueOfN;
 		this.nanosecondsToRun = nanosecondsToRun;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "\nMeasuring:\t"+name+"\nInput value:\t"+valueOfN+"\nTime taken:\t"+nanosecondsToRun+"ns\n";
