73acf6674cc7c70f1718e443eb5031a42feead95
demonikki <nsankara@ucsd.edu>
2018-02-02 12:42:40 -0800

    Revert "comment"
    This reverts commit a85b566fee04a0aa326a6e3862fc3508d3283f61.

--- pa4-student/src/cse12pa4student/Measure.java
+++ pa4-student/src/cse12pa4student/Measure.java
@@ -3,12 +3,6 @@ package cse12pa4student;
 import java.util.List;
 import static cse12pa4mysteries.Mysteries.*;
 
-/* 
- * Class to measure running time of methods
- * You can import any of the standard Java collections
- * but NOT any external libraries (which the autograder won't have)
- */
-
 public class Measure {
 
 
