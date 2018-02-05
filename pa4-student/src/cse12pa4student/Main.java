ad4e8b6e8a09c3381ba22e137a6e94618d618faa
vino <vino@128.54.173.176>
2018-02-04 22:49:19 -0800

    2018-2-4 22:48:35 updated implemented

--- pa4-student/src/cse12pa4student/Main.java
+++ pa4-student/src/cse12pa4student/Main.java
@@ -1,8 +1,13 @@
 package cse12pa4student;
+
+import java.util.List;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-			System.out.println("Remember, the code milestone is due Monday 11:59pm!");
-		}
+		List<Measurement> lst = Measure.measure
+				(new String[] { "A", "B", "C", "D", "E", "F"},1,20);
+		System.out.println(Measure.measurementsToCSV(lst));
+		//System.out.println("Remember, the code milestone is due Monday 11:59pm!");
+	}
 }
--- pa4-student/src/cse12pa4student/Measure.java
+++ pa4-student/src/cse12pa4student/Measure.java
@@ -1,5 +1,6 @@
 package cse12pa4student;
 
+import java.util.ArrayList;
 import java.util.List;
 import static cse12pa4mysteries.Mysteries.*;
 
@@ -8,16 +9,78 @@ public class Measure {
 
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
 		/** TODO **/
+		long start,end;
+		List<Measurement> lst = new ArrayList<Measurement>();
+		for (int i = 0; i < toRun.length; i++){
+			if (toRun[i].charAt(0) == 'A'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryA(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("A",j,end-start);
+					lst.add(m);
+				}
+			}
+			if (toRun[i].charAt(0) == 'B'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryB(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("B",j,end-start);
+					lst.add(m);
+				}
+			}
+			if (toRun[i].charAt(0) == 'C'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryC(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("C",j,end-start);
+					lst.add(m);
+				}
+			}
+			if (toRun[i].charAt(0) == 'D'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryD(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("D",j,end-start);
+					lst.add(m);
+				}
+			}
+			if (toRun[i].charAt(0) == 'E'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryE(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("E",j,end-start);
+					lst.add(m);
+				}
+			}
+			if (toRun[i].charAt(0) == 'F'){
+				for (int j = startN; j <= stopN; j++){
+					start = System.nanoTime();
+					mysteryF(j);
+					end = System.nanoTime();
+					Measurement m = new Measurement("F",j,end-start);
+					lst.add(m);
+				}
+			}
+		}
 		
 		//Example call to mystery method
-		mysteryA(50);
-		return null;
+		//mysteryA(50);
+		return lst;
 	}
 	
 
 	public static String measurementsToCSV(List<Measurement> toConvert) {
 		/** TODO **/
-		return null;
+		String re = new String();
+		for (Measurement m : toConvert){
+			re += m.name + "," + m.valueOfN + "," + m.nanosecondsToRun + "\n";
+		}
+		return re;
 	}
 	
 	/* Add any helper methods you find useful */
